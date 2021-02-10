a1058f5966e53214dec72e3184c4baff6d266443
Guilherme Goncalves <inacio.guilherme@gmail.com>
2021-02-10 16:09:16 -0300

    [feat] Allows to set the fallback element type

--- spec/test_helpers.rb
+++ spec/test_helpers.rb
@@ -1,10 +1,10 @@
 require 'open3'
 
 module TestHelpers
-  CONVERT_COMMAND = 'textplay -f'.freeze
+  CONVERT_COMMAND = 'textplay'.freeze
 
-  def convert_from_fountain_to_fdx(fountain_data)
-    Open3.capture2(CONVERT_COMMAND, stdin_data: fountain_data)[0]
+  def convert_from_fountain_to_fdx(fountain_data, cmd_options = ['-f'])
+    Open3.capture2("#{CONVERT_COMMAND} #{cmd_options.join(" ")}", stdin_data: fountain_data)[0]
   end
 
   def fdx_tag_with_content(tag, inner_content)
